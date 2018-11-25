22f21745571ad6bd878e1ec6719a7f2352f6b319
Hirotaka <yokokawa.491992@gmail.com>
2018-11-25 17:26:44 +0900

    app側を作った｡

---
+++ app.py
@@ -0,0 +1,24 @@
+def main():
+    print("=======Welcome to CRM Application =======")
+    print("[S]how all customers information")
+    print("[A]dd new customers")
+    print("[Q]uit")
+    print("=========================================")
+
+    command = input("Your command > ")
+
+    while command != "q":
+        if command == "s":
+            print("顧客一覧を表示します")
+        elif command == "a":
+            print("新規の顧客情報を追加します")
+        else:
+            print(f"{command}:command not found")
+
+        command = input("Your command > ")
+
+    print("Bye")
+
+
+if __name__ == "__main__":
+    main()
